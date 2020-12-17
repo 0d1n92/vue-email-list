04728899e092a6ea6098e036df0f69a858b605e3
enrico <rombaldonienrico@gmail.com>
2020-12-17 09:16:56 +0100

    add obj

--- js/script.js
+++ js/script.js
@@ -7,14 +7,25 @@ el: '#root',
 data: {
   mailList:[],
 
+
 },
+methods: {
+  pushObj: function (element) {
+    var newObj= {
+      mail:element,
+    };
+    this.mailList.push(newObj);
+  }
+},
+
 mounted: function(){
   for(let i=0; i<10;i++){
-  axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then( (log)=> {
-      this.mailList.push(log.data.response);
+    axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then( (log)=> {
+          this.pushObj(log.data.response);
     });
+        console.log(this.mailList);
   }
-  console.log( this.mailList);
+
 }
 
 })
